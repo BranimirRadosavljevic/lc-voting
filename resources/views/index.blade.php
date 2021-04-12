a3d02644ad08663068b6e86627686506bd77b1f8
Branimir Radosavljevic <rbranimir@ymail.com>
2021-04-12 13:27:44 +0200

    mid Episode 8

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div class="filters flex space-x-6">
-        <div class="w-1/3">
+    <div class="filters flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
+        <div class="w-full md:w-1/3">
             <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
                 <option value="Category One">Category One</option>
                 <option value="Category Two">Category Two</option>
@@ -8,7 +8,7 @@
                 <option value="Category Four">Category Four</option>
             </select>
         </div>
-        <div class="w-1/3">
+        <div class="w-full md:w-1/3">
             <select name="other_filters" id="other_filters" class="w-full rounded-xl border-none px-4 py-2">
                 <option value="Filter One">Filter One</option>
                 <option value="Filter Two">Filter Two</option>
@@ -16,7 +16,7 @@
                 <option value="Filter Four">Filter Four</option>
             </select>
         </div>
-        <div class="w-2/3 relative">
+        <div class="w-full md:w-2/3 relative">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -43,11 +43,11 @@
             <div class="flex flex-1 px-2 py-6">
                 <div class="flex-none">
                     <a href="#">
-                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar"
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=11" alt="avatar"
                             class="w-14 h-14 rounded-xl">
                     </a>
                 </div>
-                <div class="w-full mx-4">
+                <div class="w-full flex flex-col justify-between mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline">A random title can go here</a>
                     </h4>
@@ -108,7 +108,7 @@
             </div>
             <div class="flex px-2 py-6">
                 <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=" alt="avatar"
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar"
                         class="w-14 h-14 rounded-xl">
                 </a>
                 <div class="mx-4">
@@ -152,5 +152,126 @@
                 </div>
             </div>
         </div> <!-- end idea container-->
+
+        <div
+            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl text-blue">66</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-blue border border-blue hover:bg-blue-hover 
+                    font-bold text-xxs text-white uppercase rounded-xl transition duration-150 ease-in px-4 py-3 ">Voted</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="avatar"
+                        class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Another random title can go here</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum perferendis, nobis quia
+                        aperiam quidem earum hic. Repellendus explicabo, repudiandae minima rem reprehenderit error
+                        eligendi laborum corporis placeat numquam ut et! Iste enim, impedit ipsa esse vel adipisci odit
+                        distinctio eius! Quibusdam enim asperiores eaque consequuntur, similique quidem aspernatur! Odit
+                        cumque, perferendis iste, voluptates alias, eius illo totam velit consectetur non nisi nemo
+                        minus debitis recusandae. Optio iste illum nemo omnis molestias cupiditate itaque rem maiores,
+                        ipsum quos eaque dolores earum doloremque. Blanditiis possimus enim accusamus quasi ullam
+                        placeat, quibusdam, suscipit praesentium perspiciatis animi sit architecto doloribus natus fuga.
+                        Tempore?
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div
+                                class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 px-4 py-2">
+                                In progress
+                            </div>
+                            <button
+                                class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end idea container-->
+
+        <div
+            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl text-blue">66</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+                <div class="mt-8">
+                    <button
+                        class="w-20 bg-blue border border-blue hover:bg-blue-hover 
+                    font-bold text-xxs text-white uppercase rounded-xl transition duration-150 ease-in px-4 py-3 ">Voted</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=3" alt="avatar"
+                        class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Another random title can go here</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum perferendis, nobis quia
+                        aperiam quidem earum hic. Repellendus explicabo, repudiandae minima rem reprehenderit error
+                        eligendi laborum corporis placeat numquam ut et! Iste enim, impedit ipsa esse vel adipisci odit
+                        distinctio eius! Quibusdam enim asperiores eaque consequuntur, similique quidem aspernatur! Odit
+                        cumque, perferendis iste, voluptates alias, eius illo totam velit consectetur non nisi nemo
+                        minus debitis recusandae. Optio iste illum nemo omnis molestias cupiditate itaque rem maiores,
+                        ipsum quos eaque dolores earum doloremque. Blanditiis possimus enim accusamus quasi ullam
+                        placeat, quibusdam, suscipit praesentium perspiciatis animi sit architecto doloribus natus fuga.
+                        Tempore?
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div
+                                class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 px-4 py-2">
+                                In progress
+                            </div>
+                            <button
+                                class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end idea container-->
+
     </div> <!-- end of ideas container-->
 </x-app-layout>
