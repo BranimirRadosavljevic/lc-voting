039190fa5d25c154a796b9e5cdf5790969e2e8c1
Branimir Radosavljevic <rbranimir@ymail.com>
2021-04-14 09:17:45 +0200

    mid episode 8 20:00

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,7 +30,7 @@
     <div class="ideas-container space-y-6 my-6">
         <div
             class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl">12</div>
                     <div class="text-gray-500">Votes</div>
@@ -40,21 +40,21 @@
                     font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3 ">Vote</button>
                 </div>
             </div>
-            <div class="flex flex-1 px-2 py-6">
-                <div class="flex-none">
+            <div class="flex flex-col md:flex-row flex-1 px-2 py-6">
+                <div class="flex-none mx-4 md:mx-0">
                     <a href="#">
                         <img src="https://source.unsplash.com/200x200/?face&crop=face&v=11" alt="avatar"
                             class="w-14 h-14 rounded-xl">
                     </a>
                 </div>
                 <div class="w-full flex flex-col justify-between mx-4">
-                    <h4 class="text-xl font-semibold">
+                    <h4 class="text-xl font-semibold mt-2 md:mt-0">
                         <a href="#" class="hover:underline">A random title can go here</a>
                     </h4>
                     <div class="text-gray600 mt-3 line-clamp-3">
                         Lorem ipsum dolor sit amet consectetur
                     </div>
-                    <div class="flex items-center justify-between mt-6">
+                    <div class="flex flex-col md:flex-row md:items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                             <div>10 hours ago</div>
                             <div>&bull;</div>
@@ -63,7 +63,7 @@
                             <div class="text-gray-900">3 Comments</div>
                         </div>
                         <div x-data="{isOpen: false}"
-                             class="flex items-center space-x-2">
+                             class="flex items-center space-x-2 mt-4 md:mt-0">
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full
                                      text-center w-28 h-7 px-4 py-2">Open</div>
                             <button
@@ -79,7 +79,8 @@
                                     x-show.transition.origin.top.left="isOpen"
                                     @click.away="isOpen=false"
                                     @keydown.escape.window="isOpen=false"
-                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 ml-8">
+                                    class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl py-3 md:ml-8
+                                           top-8 md:top-6 right-0 md:left-0 ">
                                     <li><a href="#"
                                             class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark
                                             as Spam</a></li>
@@ -89,13 +90,23 @@
                                 </ul>
                             </button>
                         </div>
+
+                        <div class="flex items-center md:hidden mt-4 md:mt-0">
+                            <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                                <div class="text-sm font-bold leading-none">12</div>
+                                <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                            </div>
+                            <button class="w-20 bg-gray-200 border border-gray-200 font-bold 
+                                text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div> <!-- end idea container-->
         <div
             class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl text-blue">66</div>
                     <div class="text-gray-500">Votes</div>
@@ -155,7 +166,7 @@
 
         <div
             class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl text-blue">66</div>
                     <div class="text-gray-500">Votes</div>
@@ -215,7 +226,7 @@
 
         <div
             class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl text-blue">66</div>
                     <div class="text-gray-500">Votes</div>
