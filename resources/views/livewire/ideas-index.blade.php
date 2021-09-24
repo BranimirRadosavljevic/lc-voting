173abb729664107cf6580eb255eb54c39142b7c6
Branimir Radosavljevic <rbranimir@ymail.com>
2021-09-24 12:35:35 +0200

    Episode 30

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -24,14 +24,19 @@
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
             </div>
-            <input type="search" class="w-full rounded-xl bg-white px-4 py-2 pl-8 border-none placeholder-gray-900"
+            <input wire:model="search" type="search" class="w-full rounded-xl bg-white px-4 py-2 pl-8 border-none placeholder-gray-900"
                 placeholder="Find an idea">
         </div>
     </div> <!-- end of filters-->
     <div class="ideas-container space-y-6 my-6">
-        @foreach ($ideas as $idea)
+        @forelse ($ideas as $idea)
         <livewire:idea-index :key="$idea->id" :idea="$idea" :votesCount="$idea->votes_count" />
-        @endforeach
+        @empty 
+            <div class="mx-auto w-70 mt-12">
+                <img src="{{ asset('img/no-ideas.svg')}}" alt="No Ideas" class="mx-auto" style="mix-blend-mode: luminosity">
+                <div class="text-gray-400 text-center font-bold mt-6">No ideas were found...</div>
+            </div>
+        @endforelse
 
     </div> <!-- end of ideas container-->
 
