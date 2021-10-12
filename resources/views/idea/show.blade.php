8c2c77f3b32d8ada4cf577c087f59d883d6236b8
Branimir Radosavljevic <rbranimir@ymail.com>
2021-10-12 13:24:11 +0200

    Episode 46

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -10,33 +10,7 @@
 
     <livewire:idea-show :idea="$idea" :votesCount="$votesCount" />
 
-    <div x-data="{ isOpen: false}">
-        <button @click="isOpen=!isOpen">Toggle</button>
-        <div
-            x-cloak
-            x-show="isOpen"
-            @keydown.escape.window="isOpen = false"
-            x-init="
-                window.livewire.on('blablabla', () => {
-                    isOpen = false
-                })
-            "
-            class="z-20 flex justify-between max-w-xs sm:max-w-sm w-full fixed bottom-0 right-0
-                    bg-white rounded-xl shadow-lg border px-6 py-5 mx-2 sm:mx-6 my-8">
-            <div class="flex items-center">
-                <svg class="text-green h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                <span class="font-semibold text-gray-500 text-sm sm:text-base ml-2">Idea was updated successfully!</span>
-            </div>
-            <button class="text-gray-400 hover:text-gray-500" type="button">
-                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-        </div>
-    </div>
+    <x-notification-success />
 
     <x-modals-container :idea="$idea" />
 
